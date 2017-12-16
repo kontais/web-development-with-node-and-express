8a50110123d86505fc8a7b5dd3812ff38e02d507
kontais <kontais@aliyun.com>
2017-12-16 10:47:12 +0800

    cluster

--- ch04/meadowlark.js
+++ ch04/meadowlark.js
@@ -1,3 +1,4 @@
+var http = require('http');
 var express = require('express');
 var fortune = require('./lib/fortune.js');
 
@@ -45,7 +46,20 @@ app.use(function(err, req, res, next){
 	res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' + 
-    app.get('port') + '; press Ctrl-C to terminate.' );
-});
+var server;
+
+function startServer() {
+    server = http.createServer(app).listen(app.get('port'), function(){
+      console.log( 'Express started in ' + app.get('env') +
+        ' mode on http://localhost:' + app.get('port') +
+        '; press Ctrl-C to terminate.' );
+    });
+}
+
+if(require.main === module){
+    // application run directly; start app server
+    startServer();
+} else {
+    // application imported as a module via "require": export function to create server
+    module.exports = startServer;
+}
