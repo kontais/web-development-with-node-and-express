e9f03db3935171de1645faa86f4c20121068a466
kontais <kontais@aliyun.com>
2017-12-16 10:50:29 +0800

    cluster

--- ch04/meadowlark.js
+++ ch04/meadowlark.js
@@ -16,10 +16,14 @@ app.use(express.static(__dirname + '/public'));
 
 // set 'showTests' context property if the querystring contains test=1
 app.use(function(req, res, next){
-	console.log("app.get('env')=" + app.get('env'));
-	console.log("req.query.test=" + req.query.test);
+	// console.log("app.get('env')=" + app.get('env'));
+	// console.log("req.query.test=" + req.query.test);
 	res.locals.showTests = app.get('env') !== 'production' && 
 		req.query.test === '1';
+
+	var cluster = require('cluster');
+	if(cluster.isWorker) console.log('Worker %d received request', cluster.worker.id);
+
 	next();
 });
 
